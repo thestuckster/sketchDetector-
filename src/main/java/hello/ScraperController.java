00957f5d0f71a432d2d172708c1bb999d2f54449
thestuckster <sstucky3@gmail.com>
2015-07-22 17:49:07 -0500

    tryig to fix CORS bug

--- src/main/java/hello/ScraperController.java
+++ src/main/java/hello/ScraperController.java
@@ -3,15 +3,16 @@ package hello;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ReponseBody;
+import org.springframework.stereotype.Controller;
 
 
-@RestController
+@Controller
 public class ScraperController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/scrape")
-    public Scraper scraper(@RequestParam(value="url", defaultValue = "") String Url) {
+    public @ReponseBody Scraper scraper(@RequestParam(value="url", defaultValue = "") String Url) {
         return new Scraper(counter.incrementAndGet(), Url);
     }
 }
