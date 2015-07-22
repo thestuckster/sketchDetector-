2d465764e2e9bdbd9f964637bb802c6a7c8421a1
thestuckster <sstucky3@gmail.com>
2015-07-22 17:58:23 -0500

    debugging

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
+			var scrape = "localhost:8080/scrape";
+			$.ajax({
+		    url: "http://localhost:8080/scrape",
+		    type: 'GET',
+		    success: function() { alert('PUT completed'); }
+		});
+
+
 	$("button").click(function() {
 		var url = $("#url").val();
-		var scrape = "localhost:8080/scrape?url=" + url;
+		var scrape = "localhost:8080/scrape";
 
-		$.get(scrape, function(data) {
+		$.get(scrape,
+			 {url: "http://stucky.me"},
+			 function(data) {
 	  		alert(data);
 		});
 	});
