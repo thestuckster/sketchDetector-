ef4e16ad1702b9acfebbf3caddf8a9c273d69a28
thestuckster <sstucky3@gmail.com>
2015-07-22 18:00:07 -0500

    debugging

--- src/main/java/hello/ScraperController.java
+++ src/main/java/hello/ScraperController.java
@@ -3,16 +3,15 @@ package hello;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ReponseBody;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class ScraperController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/scrape")
-    public @ReponseBody Scraper scraper(@RequestParam(value="url", defaultValue = "") String Url) {
+    public Scraper scraper(@RequestParam(value="url", defaultValue = "") String Url) {
         return new Scraper(counter.incrementAndGet(), Url);
     }
 }
