281f048ce614ffbfa92da7ac12591e9ebba8a863
thestuckster <sstucky3@gmail.com>
2015-07-23 11:04:14 -0500

    debugging

--- src/main/java/hello/CORSFilter.java
+++ src/main/java/hello/CORSFilter.java
@@ -6,6 +6,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +16,7 @@ public class CORSFilter implements Filter {
 public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) 
     throws IOException, ServletException {
     HttpServletResponse response = (HttpServletResponse) res;
-    HttpServletRequest request = (HttpServletRequest)req;
+    HttpServletRequest  request = (HttpServletRequest)req;
 
     response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
     response.setHeader("Access-Control-Allow-Methods", 
