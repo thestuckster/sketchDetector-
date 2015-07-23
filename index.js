50a5ebaef5a747a77fee3e7dac765c46cee63f9f
thestuckster <sstucky3@gmail.com>
2015-07-22 21:55:55 -0500

    notifies user of level of sketchyness

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
-$(document).ready(function() {
-	$("button").click(function() {
-		var url = $("#url").val();
-		var scrape = "http://localhost:8080/scrape?url=" + url;
+function getCount(obj) {
+	var count = 0;
+	console.warn("DO I GET FIRED???");
 
-		$.ajax({
-			url: scrape,
-			type: 'GET',
-			success: function(data) {
-				console.log(data);
-			}
-	});
+	if (obj["containsDocumentWrite"] === true) {
+		count++;
+		console.log(count);
+	}
+	if (obj["containsEval"] === true) {
+		count++;
+		console.log(count);
+	}
+	if (obj["containsIFrame"] === true) {
+		count++;
+		console.log(count);
+	}
+	return count;
+}
+
+$(document).ready(function() {
+    $("button").click(function() {
+      var url = $("#url").val();
+      var scrape = "http://localhost:8080/scrape?url=" + url;
+
+      $.ajax({
+        url: scrape,
+        type: 'GET',
+        success: function(data) {
+					console.log(JSON.stringify(data));
+
+          var count = 0;
+
+					$.each(data, function(key, value) {
+						if(typeof value === "boolean" && value === true) {
+							count = count + 1;
+						}
+					});
+
+					console.log(count);
+
+          if (count === 0) {
+            alert("This site is safe!");
+          } else if (count === 1) {
+            alert("This site is slightly sketchy");
+          } else if (count === 2) {
+            alert("This site is moderately sketchy");
+          } else {
+            alert("This site SUPER sketchy!!!!1");
+          }
+        }
+    });
 	});
 });
