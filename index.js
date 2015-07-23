2d9357f18122f84eaa88a73f5c3c1e45ef3e6132
mbrown9792 <mbrown9792@gmail.com>
2015-07-23 13:32:48 -0500

    Added when enter key is pressed

--- index.js
+++ index.js
@@ -29,4 +29,36 @@ $(document).ready(function() {
       }
     });
   });
+
+  $("url").bind("keypress",function(e) {
+    if(e.keyCode == 13) {
+      var url = $("#url").val();
+      var scrape = "http://localhost:8080/scrape?url=" + url;
+
+      $.ajax({
+        url: scrape,
+        type: 'GET',
+        success: function(data) {
+          console.log(JSON.stringify(data));
+          var count = 0;
+
+          $.each(data, function(key, value) {
+            if (typeof value === "boolean" && value === true) {
+              count = count + 1;
+            }
+          });
+        
+          if (count === 0) {
+            alert("This site is safe!");
+          } else if (count === 1) {
+            alert("This site is slightly sketchy");
+          } else if (count === 2) {
+            alert("This site is moderately sketchy");
+          } else {
+            alert("This site SUPER sketchy!!!!1");
+          }
+        }
+      });
+    }
+  });
 });
