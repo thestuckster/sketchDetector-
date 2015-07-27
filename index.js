1c5b45663c3737f9034be28182eda6fa47dd6fef
thestuckster <sstucky3@gmail.com>
2015-07-27 18:59:44 -0500

    cleanup

--- index.js
+++ index.js
@@ -9,38 +9,12 @@ $(document).ready(function() {
       success: function(data) {
         console.log(JSON.stringify(data));
 
-        var count = 0;
-
-        $.each(data, function(key, value) {
-          if (typeof value === "boolean" && value === true) {
-            count = count + 1;
-          }
-        });
-
-        if (count === 0) {
-          alert("This site is safe!");
-        } else if (count === 1) {
-          alert("This site is slightly sketchy");
-        } else if (count === 2) {
-          alert("This site is moderately sketchy");
+        if (data.source === "bad connection to site") {
+          alert("bad connection to site");
+        } else if (data.source === "Malformed URL") {
+          alert("Bad URL. Please try again.");
         } else {
-          alert("This site SUPER sketchy!!!!1");
-        }
-      }
-    });
-  });
 
-  $("body").keypress(function(e) {
-    if(e.which == 13) {
-      console.log("ENTER");
-      var url = $("#url").val();
-      var scrape = "http://localhost:8080/scrape?url=" + url;
-
-      $.ajax({
-        url: scrape,
-        type: 'GET',
-        success: function(data) {
-          console.log(JSON.stringify(data));
           var count = 0;
 
           $.each(data, function(key, value) {
@@ -57,6 +31,46 @@ $(document).ready(function() {
             alert("This site is moderately sketchy");
           } else {
             alert("This site SUPER sketchy!!!!1");
+          }
+        }
+      }
+    });
+  });
+
+  $("body").keypress(function(e) {
+    if (e.which == 13) {
+      var url = $("#url").val();
+      var scrape = "http://localhost:8080/scrape?url=" + url;
+
+      $.ajax({
+        url: scrape,
+        type: 'GET',
+        success: function(data) {
+          console.log(JSON.stringify(data));
+
+          if (data.source === "bad connection to site") {
+            alert("bad connection to site");
+          } else if (data.source === "Malformed URL") {
+            alert("Bad URL. Please try again.");
+          } else {
+
+            var count = 0;
+
+            $.each(data, function(key, value) {
+              if (typeof value === "boolean" && value === true) {
+                count = count + 1;
+              }
+            });
+
+            if (count === 0) {
+              alert("This site is safe!");
+            } else if (count === 1) {
+              alert("This site is slightly sketchy");
+            } else if (count === 2) {
+              alert("This site is moderately sketchy");
+            } else {
+              alert("This site SUPER sketchy!!!!1");
+            }
           }
         }
       });
